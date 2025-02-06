958df69d8bb0ee7b486f2e42ad93bc249759d854
Akshat Singh <77851070+Akshatsingh14@users.noreply.github.com>
2025-02-06 06:20:13 +0530

    Update settings.py

--- Dominos_Clone/settings.py
+++ Dominos_Clone/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'saiyajinakshat14@gmail.com'
-EMAIL_HOST_PASSWORD = 'fyus vdlp qcvd dgro'
+EMAIL_HOST_USER = 'use your email'
+EMAIL_HOST_PASSWORD = 'your email pswrd key'
